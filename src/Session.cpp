fe713c29d0af6e20bff4660170c6f29efa0bafbb
Didar Oreshev <doreshev@3-B-3.42heilbronn.de>
2023-02-08 11:45:11 +0100

    session changed to requested

--- src/Session.cpp
+++ src/Session.cpp
@@ -6,7 +6,7 @@
 /*   By: doreshev <doreshev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 18:13:52 by doreshev          #+#    #+#             */
-/*   Updated: 2023/02/07 19:57:08 by doreshev         ###   ########.fr       */
+/*   Updated: 2023/02/08 11:41:51 by doreshev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,33 @@ Session::~Session() {}
 
 //MEMBER FUNCTIONS
 int Session::create(const std::string& username, const std::string& password) {
-    if (user_exists(username))
-        return EXIT_FAILURE;
-    int key = generate_key();
+    int key = get_key(username);
+
+    if (key == true) {
+        if (password_correct(key, password) == false)
+            return false;
+        return key;
+    }
+
+    key = generate_key();
     _cookies[key] = User(username, password);
-    return  key;
+    return key;
+}
+
+int    Session::get_key(const std::string& username) const {
+    for (std::map<int, User>::iterator it = _cookies.begin(); it != _cookies.end(); it++) {
+        if (it->second.name == username) {
+            return it->first;
+    }
+    return false;
+}
+
+bool    Session::password_correct(int key, const std::string& password) const {
+    std::map<int, User>::iterator it = _cookies.find(key);
+
+    if (it != _cookies.end() && it->second.pass == password)
+        return true;
+    return false;
 }
 
 int     Session::generate_key() {
@@ -36,30 +58,9 @@ int     Session::generate_key() {
     return key;
 }
 
-bool    Session::user_exists(const std::string& username) {
-    for (std::map<int, User>::iterator it = _cookies.begin(); it != _cookies.end(); it++) {
-        if (it->second.name == username)
-            return true;
-    }
-    return false;
-}
 
-int    Session::logout(const int& key) {
-    std::map<int, User>::iterator it = _cookies.find(key);
-    
-    if (it == _cookies.end())
-        return EXIT_FAILURE;
-    it->second.logged = false;
-    return EXIT_SUCCESS;
-}
 
-std::string    Session::last_access(const int& key) const {
-    std::map<int, User>::const_iterator it = _cookies.find(key);
-    
-    if (it == _cookies.end())
-        return "";
-    return it->second.last_access;
-}
+
 
 std::string    Session::username(const int& key) const {
     std::map<int, User>::const_iterator it = _cookies.find(key);
@@ -67,14 +68,6 @@ std::string    Session::username(const int& key) const {
     if (it == _cookies.end())
         return "";
     return it->second.name;
-}
-
-bool    Session::logged(const int& key) const {
-    std::map<int, User>::const_iterator it = _cookies.find(key);
-    
-    if (it == _cookies.end())
-        return false;
-    return it->second.logged;
 }
 
 void    Session::delete_session(const int& key) {
