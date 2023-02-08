470acb9700715cde560d6d584f623d0e3401b2c7
Didar Oreshev <doreshev@3-B-3.42heilbronn.de>
2023-02-08 11:46:00 +0100

    session changed to requested

--- incl/Session.hpp
+++ incl/Session.hpp
@@ -6,28 +6,21 @@
 /*   By: doreshev <doreshev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 18:12:06 by doreshev          #+#    #+#             */
-/*   Updated: 2023/02/07 19:57:20 by doreshev         ###   ########.fr       */
+/*   Updated: 2023/02/08 11:42:02 by doreshev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
-#include <map>
+#include "defines.hpp"
 
-struct User
-{
+struct User {
     std::string                                         name;
     std::string                                         pass;
-    std::string                                         last_access;
-    bool                                                logged;
 
     User() {}
-    User(const std::string& username, const std::string& password) : name(username),
-            pass(password), logged(true) {
-        time_t  current_time = time(0);
-        last_access = ctime(&current_time);
-    }
+    User(const std::string& username, const std::string& password) 
+        : name(username), pass(password) {}
 };
 
 class Session {
@@ -39,16 +32,11 @@ public:
 	~Session();
 
 	int 			create(const std::string& username, const std::string& password);
-    int             logout(const int& key);
-    
-    
     std::string     username(const int& key) const;
-    std::string     last_access(const int& key) const;
-    bool            logged(const int& key) const;
-    
     void            delete_session(const int& key);
 
 private:
+    int             get_key(const std::string& username) const;
+    bool            password_correct(int key, const std::string& password) const;
     int             generate_key();
-    bool            user_exists(const std::string& username);
 };
