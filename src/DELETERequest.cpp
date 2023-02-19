b93e31f5e7b2c7c521dfac1d3b224ae22c714ed3
FVNRLS <rmazurit@student.42heilbronn.de>
2023-02-19 17:04:17 +0100

    DELETE method fixed for postman

--- src/DELETERequest.cpp
+++ src/DELETERequest.cpp
@@ -17,12 +17,11 @@ int DELETERequest::create_response(const request_handler &request) {
 	size_t end = request.buf.find("\"", begin);
 	if (end == std::string::npos)
 		return BAD_REQUEST;
-	filepath = "uploads" + request.buf.substr(begin, end - begin);
+	filepath = "uploads/" + request.buf.substr(begin, end - begin);
 
 	if (access(filepath.c_str(), F_OK) < 0 )
 		return PAGE_NOT_FOUND;
 	if (unlink(filepath.c_str()) == -1)
 		return FORBIDDEN;
-
 	return OK;
 }
